b83acce04b134d14e680f1ae9b17604921eb16bb
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-19 00:58:31 -0600

    edits

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const jest = require("jest");
 const path = require("path");
+const fs = require("fs");
 
 // Global Variables
 let primaryKey = 0;
 var teamMembers = [];
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 // const { rejects } = require("assert");
 // const { resolve } = require("path");
 // const { createPromptModule } = require("inquirer");
@@ -26,51 +28,81 @@ var teamMembers = [];
 // }
 
 function questions() {
-  function allTeamMmbers() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What type of team-member would you like to add?",
+        choices: ["Engineer", "Intern", "Managment", "Exit"],
+      },
+    ])
+    .then((user) => {
+      switch (user.choice) {
+        case "Engineer":
+          engineerDetails();
+          break;
+        case "Intern":
+          internDetails();
+          break;
+        case "Managment":
+          managerDetails();
+          break;
+        default:
+          teamBuild(); // Need to create this method.
+          break;
+      }
+    });
+  // .then((engrRes) => {
+  //   var nxtEngineer = new Engineer(
+  //     engrRes.email,
+  //     engrRes.name,
+  //     engrRes.github
+  //   );
+  //   primaryKey++;
+  //   teamMembers.push(nxtEngineer);
+  // });
+  // .then((internResponse) => {
+  //   var nxtIntern = new Intern(
+  //     primaryKey,
+  //     internResponse.email,
+  //     internResponse.name,
+  //     internResponse.college
+  //   );
+  //   primaryKey++;
+  //   teamMembers.push(nxtIntern);
+  // });
+  // .then((managmentResponse) => {
+  //   var nxtManager = new Manager(
+  //     managmentResponse.email,
+  //     managmentResponse.name,
+  //     managmentResponse.officeNumber
+  //   );
+  //   primaryKey++;
+  //   teamMembers.push(nxtManager);
+  // });
+  teamBuild();
+
+  function managerDetails() {
+    console.log("ndjwsndjsnfjkf");
     inquirer
       .prompt([
         {
-          type: "list",
-          name: "choice",
-          message: "What type of team-member would you like to add?",
-          choices: ["Engineer", "Intern", "Managment", "Exit"],
+          type: "input",
+          name: "name",
+          message: "What is the manager's name?",
+        },
+        {
+          type: "input",
+          name: "room",
+          message: "What is the office number?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your e-mail?",
         },
       ])
-      .then((user) => {
-        switch (user.choice) {
-          case "Engineer":
-            engineerDetails();
-            break;
-          case "Intern":
-            internDetails();
-            break;
-          case "Managment":
-            managerDetails();
-            break;
-          default:
-            teamBuild(); // Need to create this method.
-            break;
-        }
-      })
-      .then((engrRes) => {
-        var nxtEngineer = new Engineer(
-          engrRes.email,
-          engrRes.name,
-          engrRes.github
-        );
-        primaryKey++;
-        teamMembers.push(nxtEngineer);
-      })
-      .then((internResponse) => {
-        var nxtIntern = new Intern(
-          primaryKey,
-          internResponse.email,
-          internResponse.name,
-          internResponse.college
-        );
-        primaryKey++;
-        teamMembers.push(nxtIntern);
-      })
       .then((managmentResponse) => {
         var nxtManager = new Manager(
           managmentResponse.email,
@@ -79,50 +111,8 @@ function questions() {
         );
         primaryKey++;
         teamMembers.push(nxtManager);
+        questions();
       });
-    teamBuild();
-  }
-
-  function managerDetails() {
-    console.log("ndjwsndjsnfjkf");
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the manager's name?",
-
-        validate: function name(answer) {
-          if (name === answer) {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
-        },
-      },
-      {
-        type: "input",
-        name: "room",
-        message: "What is the office number?",
-
-        validate: function room(answer) {
-          if (room === answer) {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
-        },
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your e-mail?",
-
-        validate: function email(answer) {
-          if (email === answer) {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
-        },
-      },
-    ]);
     // .then((answer) => {
     //   var manager = new Manager(
     //     answer.managerName,
@@ -132,92 +122,94 @@ function questions() {
     //     teamMembers.push(manager)
     //   );
     // });
-    allTeamMmbers();
+    // allTeamMmbers();
   }
   //   managerDetails();
 
   function internDetails() {
-    console.log("njkenfjdnfj");
-    inquirer.prompt([
-      {
-        type: "input",
-        message: "What is your name?",
-        name: "name",
-        validate: function name() {
-          if (name === "") {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
+    // console.log("njkenfjdnfj");
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is your name?",
+          name: "name",
         },
-      },
-      {
-        type: "input",
-        message: "What school are you currently attending?",
-        name: "college",
-        validate: function college() {
-          if (college === "") {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
+        {
+          type: "input",
+          message: "What school are you currently attending?",
+          name: "college",
         },
-      },
-      {
-        type: "input",
-        message: "What is your e-mail?",
-        name: "email",
-        validate: function email() {
-          if (email === "") {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
+        {
+          type: "input",
+          message: "What is your e-mail?",
+          name: "email",
         },
-      },
-    ]);
+      ])
+      .then((internResponse) => {
+        var nxtIntern = new Intern(
+          primaryKey,
+          internResponse.email,
+          internResponse.name,
+          internResponse.college
+        );
+        primaryKey++;
+        teamMembers.push(nxtIntern);
+        questions();
+      });
   }
-  allTeamMmbers();
+  // allTeamMmbers();
 
-  questions();
+  // questions();
 
-  managerDetails();
+  // managerDetails();
 
   function engineerDetails() {
-    inquirer.prompt([
-      {
-        type: "input",
-        message: "What is the manager's name?",
-        name: "name",
-        validate: function name() {
-          if (name === "") {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the manager's name?",
+          name: "name",
         },
-      },
-      {
-        type: "input",
-        message: "What is your Github account name?",
-        name: "github",
-        validate: function github() {
-          if (github === "") {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
+        {
+          type: "input",
+          message: "What is your Github account name?",
+          name: "github",
         },
-      },
-      {
-        type: "input",
-        message: "What is your e-mail?",
-        name: "email",
-        validate: function email() {
-          if (email === "") {
-            return false;
-          }
-          return "Please enter in a valid answer to the question.";
+        {
+          type: "input",
+          message: "What is your e-mail?",
+          name: "email",
         },
-      },
-    ]);
-    allTeamMmbers();
+      ])
+      .then((engrRes) => {
+        var nxtEngineer = new Engineer(
+          engrRes.email,
+          engrRes.name,
+          engrRes.github
+        );
+        primaryKey++;
+        teamMembers.push(nxtEngineer);
+        questions();
+      });
+    // allTeamMmbers();
   }
+
+  function addEmployee() {
+    inquirer
+      .prompt({
+        message: "do you wish to add Employees and/or users?",
+        type: "confirm",
+        name: "createMembers",
+      })
+      .then(function ({ createMembers }) {
+        if (createMembers) {
+          questions();
+        }
+      });
+  }
+  addEmployee();
 }
 questions();
 
